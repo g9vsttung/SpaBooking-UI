647f3eebcd0c4425d2df0d5142c7803b2e9a59f9
linhtnl <lamlinh20001@gmail.com>
2021-09-16 07:11:53 +0700

    Login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:spa_booking/Screens/Login/login_screen.dart';
+import 'package:spa_booking/constants.dart';
 
 void main() {
   runApp(
-      MaterialApp());
+      MyApp()
+  );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Spa Booking',
+      theme: ThemeData(
+        primaryColor: kPrimaryBackgroudColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: LoginScreen(),
+    );
+  }
+
+}
+
+
